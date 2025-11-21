63881ea41b55c11831f423d9599a65610b0417f7
aiera2876 <aiera2876@gmail.com>
2025-11-21 09:05:46 +0545

    code

--- lib/identity_card_view.dart
+++ lib/identity_card_view.dart
@@ -8,11 +8,29 @@ class IdentityCardView extends StatefulWidget {
 }
 
 class _IdentityCardViewState extends State<IdentityCardView> {
+
+
+  textWidget(String title, String subtitle){
+    return RichText(
+      text:TextSpan(
+      text:title,
+      style: TextStyle(color: Colors.blueGrey, fontSize: 16),
+        children: [
+          TextSpan(
+            text: '',
+            color: Colors.blueGrey ,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ],
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: text(
+        title: Text(
           'My Card',
           style: TextStyle(color: Colors.purpleAccent, fontSize: 27),
         ),
@@ -28,23 +46,40 @@ class _IdentityCardViewState extends State<IdentityCardView> {
           border: Border.all(color: Colors.cyanAccent),),
           child: Row(
             children: [
-           Container(
-             height: 150,
-             width: 150,
-             decoration: BoxDecoration(
-               image: DecorationImage(image: AssetImage('assests/images/starngersthings.jpg'),
-               fit: BoxFit.cover,
+           Flexible(
+             child: Container(
+               height: 150,
+               width: 150,
+               decoration: BoxDecoration(
+                 image: DecorationImage(image: AssetImage('assests/images/starngersthings.jpg'),
+                 fit: BoxFit.cover,
+                 ),
+                 shape: BoxShape.circle,
+                 //borderRadius: BorderRadius.all(Radius.circular(12)), just to make the image rectangle as it is
                ),
-               shape: BoxShape.circle,
-               //borderRadius: BorderRadius.all(Radius.circular(12)), just to make the image rectangle as it is
              ),
            ),
               ],
             ),
           ),
-        sizedBox(width: 20),
+      ),
+    SizedBox(width: 20),
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 10,
+            children: [
+              textWidget('Name:' ,'Anu'),
+          textWidget('Age:', '21'),
+            textWidget('Address:', 'Bhaktapur'),
+              textWidget('Level:' ,'BCA'),
+              textWidget('Phone:' , '+977-98000000'),
+              ],
+          ),
         ),
-      )
+
+        );
 
   }
 }
