0c24c1b4c1536f19c872eb6562e4123d7ce3cd0c
aiera2876 <aiera2876@gmail.com>
2025-11-25 08:14:20 +0545

    code

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,101 +1,108 @@
 import 'package:flutter/material.dart';
-class LoginScreen extends StatefulWidget {
+
+
+  class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
-}
+  }
 
-class _LoginScreenState extends State<LoginScreen> {
+  class _LoginScreenState extends State<LoginScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   bool isObscure = true;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xffEFECE3),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 60,
-          left: 20,
-          right: 20,
-          bottom: 20,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset('assets/images/logo.png', width: 200),
-            SizedBox(height: 12),
-            Text('Welcome Back,', style:AppTextStyle.PoppinsMedium.copyWith(fontSize: 28)),
-            Text(
-              'Make it work, make it right, make it fast.',
-              style: AppTextStyle.PoppinsMedium.copyWith(fontSize: 18)),
-      ),
-      SizedBox(height:30),
-      TextFormField(
-        style: AppTextStyle.poppinsRegular.copywith(fontSize : 14),
-        keyboardType: TextInputType.emailAddress,
-        controller: emailController,
-        decoration : InputDecoration(hintText: 'E-mail', hintStyle: AppTextStyle.poppinsRegular.copywith(
-          fontSize: 14,
-          color : Colors.black.withOpacity(0.6),
-        ),
-        border: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey, width: 1),
-          borderRadius: BorderRadius.all(Radius.circular(8)),
-        ),
-
-          focusedBorder:  OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1),
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-      ),
-          contentPadding: EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 10,
-          ),
-          prefixIcon: Icon(Icons.person, size: 29,),
-        ),
-      ),
-      SizedBox(height: 14),
-      TextFormField(
-        style: AppTextStyle.poppinsRegular.copywith(fontSize : 14),
-
-        controller: passwordController,
-        obscureText: isObscure,
-        decoration : InputDecoration(hintText: 'password', hintStyle: AppTextStyle.poppinsRegular.copywith(
-          fontSize: 14,
-          color : Colors.black.withOpacity(0.6),
-        ),
-          border: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1),
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-          ),
-
-          focusedBorder:  OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1),
-            borderRadius: BorderRadius.all(Radius.circular(8)),
-          ),
-          contentPadding: EdgeInsets.symmetric(
-            horizontal: 10,
-            vertical: 10,
-          ),
-          prefixIcon: Icon(Icons.fingerprint, size: 29,),
-          suffixIcon: IconButton(
-            padding: EdgeInsets.zero,
-            onPressed: (), {
-              setState(() {
-                isObscure = !isObscure;
-              });
-          } ,
-              icon: Icon(Icons.visibility_off),
-          ),
-        ),
-      ),
-
-          ],
-        ),
-      ),
-
+  return Scaffold(
+  backgroundColor: Color(0xffEFECE3),
+  body: Padding(
+  padding: const EdgeInsets.only(
+  top: 60,
+  left: 24,
+  right: 24,
+  bottom: 20,
+  ),
+  child: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: [
+  Image.asset('assets/images/logo.png', width: 200),
+  SizedBox(height: 12),
+  Text(
+  'Welcome Back,',
+  style: AppTextStyle.poppinsBold.copyWith(fontSize: 28),
+  ),
+  Text(
+  'Make it work, make it right, make it fast.',
+  style: AppTextStyle.poppinsMedium.copyWith(fontSize: 16),
+  ),
+  SizedBox(height: 30),
+  TextFormField(
+  style: AppTextStyle.poppinsRegular.copyWith(fontSize: 16),
+  keyboardType: TextInputType.emailAddress,
+  controller: emailController,
+  decoration: InputDecoration(
+  hintText: 'E-Mail',
+  hintStyle: AppTextStyle.poppinsRegular.copyWith(
+  fontSize: 16,
+  color: Colors.black.withOpacity(0.6),
+  ),
+  border: OutlineInputBorder(
+  borderSide: BorderSide(color: Colors.grey, width: 1),
+  borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  focusedBorder: OutlineInputBorder(
+  borderSide: BorderSide(color: Colors.grey, width: 1),
+  borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  contentPadding: EdgeInsets.symmetric(
+  horizontal: 10,
+  vertical: 20,
+  ),
+  prefixIcon: Icon(Icons.person, size: 26),
+  ),
+  ),
+  SizedBox(height: 14),
+  TextFormField(
+  style: AppTextStyle.poppinsRegular.copyWith(fontSize: 16),
+  controller: passwordController,
+  obscureText: isObscure,
+  decoration: InputDecoration(
+  hintText: 'Password',
+  hintStyle: AppTextStyle.poppinsRegular.copyWith(
+  fontSize: 16,
+  color: Colors.black.withOpacity(0.6),
+  ),
+  border: OutlineInputBorder(
+  borderSide: BorderSide(color: Colors.grey, width: 1),
+  borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  focusedBorder: OutlineInputBorder(
+  borderSide: BorderSide(color: Colors.grey, width: 1),
+  borderRadius: BorderRadius.all(Radius.circular(8)),
+  ),
+  contentPadding: EdgeInsets.symmetric(
+  horizontal: 10,
+  vertical: 20,
+  ),
+  prefixIcon: Icon(Icons.fingerprint, size: 26),
+  suffixIcon: IconButton(
+  padding: EdgeInsets.zero,
+  onPressed: () {
+  setState(() {
+  isObscure = !isObscure;
+  });
+  },
+  icon: Icon(
+  isObscure ? Icons.visibility_off : Icons.visibility,
+  ),
+  ),
+  ),
+  ),
+  ],
+  ),
+  ),
+  );
   }
-}
+  }
