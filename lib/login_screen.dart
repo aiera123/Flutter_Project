a40cedfb0708545cbaba43ea25b4c0372ee3192f
aiera2876 <aiera2876@gmail.com>
2025-11-27 09:00:27 +0545

    loginactivity

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amine/app_routes.dart';
 import 'package:flutter/material.dart';
 import 'app_text_styles.dart';
 
@@ -12,178 +13,208 @@ class _LoginScreenState extends State<LoginScreen> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   bool isObscure = true;
+  final formkey = GlobalKey<FormState>();
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffF3E8DF),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(top: 60, left: 20, right: 20, bottom: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                'assets/images/logo.png',
-                width: 200,
-              ),
-              SizedBox(height: 12),
-              Text(
-                'Welcome Back',
-                style: AppTextStyle.poppinsBold.copyWith(fontSize: 28),
-              ),
-              Text(
-                'Make it work, Make it right, Make it fast',
-                style: AppTextStyle.poppinsSemiBold.copyWith(fontSize: 16),
-              ),
-              SizedBox(height: 30),
-              TextFormField(
-                style: AppTextStyle.poppinsRegular.copyWith(fontSize: 14),
-                keyboardType: TextInputType.emailAddress,
-                controller: emailController,
-                decoration: InputDecoration(
-                  hintText: 'E-Mail',
-                  hintStyle: AppTextStyle.poppinsRegular.copyWith(
-                    fontSize: 14,
-                    color: Colors.black.withOpacity(0.6),
-                  ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 1),
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 1),
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 20,
-                  ),
-                  prefixIcon: Icon(Icons.person, size: 26),
+          child: Form(
+            key: formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/logo.png',
+                  width: 200,
                 ),
-              ),
-              SizedBox(height: 14),
-              TextFormField(
-                style: AppTextStyle.poppinsRegular.copyWith(fontSize: 14),
-                controller: passwordController,
-                obscureText: isObscure,
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  hintStyle: AppTextStyle.poppinsRegular.copyWith(
-                    fontSize: 14,
-                    color: Colors.black.withOpacity(0.6),
-                  ),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 1),
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey, width: 1),
-                    borderRadius: BorderRadius.all(Radius.circular(8)),
-                  ),
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 20,
-                  ),
-                  prefixIcon: Icon(Icons.fingerprint, size: 26),
-                  suffixIcon: IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      setState(() {
-                        isObscure = !isObscure;
-                      });
-                    },
-                    icon: Icon(isObscure ? Icons.visibility_off : Icons.visibility),
-                  ),
+                SizedBox(height: 12),
+                Text(
+                  'Welcome Back',
+                  style: AppTextStyle.poppinsBold.copyWith(fontSize: 28),
                 ),
-              ),
-
-              SizedBox(height: 10),
-              Align(
-                alignment:Alignment.centerRight,
-                child: TextButton(onPressed: () {}, child: Text('Forgot Password?',
-                  style: AppTextStyle.poppinsMedium.copyWith(
-                    color: Colors.blue,
-                    fontSize: 16,
-                  ),
+                Text(
+                  'Make it work, Make it right, Make it fast',
+                  style: AppTextStyle.poppinsSemiBold.copyWith(fontSize: 16),
                 ),
-                ),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
-                  ),
-                  minimumSize: Size(double.infinity,56),
-                ),
-                onPressed: () {},
-                child: Text('Login'.toUpperCase(),
-                  style: AppTextStyle.poppinsMedium.copyWith(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Align(
-                alignment: Alignment.center,
-                child: Text(
-                  'or'.toUpperCase(),
-                  style: AppTextStyle.poppinsRegular.copyWith(
-                    color: Colors.black,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              OutlinedButton(
-                style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
-                  ),
-                  minimumSize: Size(double.infinity,60),
-                  side: BorderSide(color: Colors.black,width: 1.5),
-                ),
-                onPressed: () {},
-                child: Row(
-                  mainAxisAlignment:MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/images/google-logo.png',
-                      width: 26,
-                      height: 26,
+                SizedBox(height: 30),
+                TextFormField(
+                  style: AppTextStyle.poppinsRegular.copyWith(fontSize: 14),
+                  keyboardType: TextInputType.emailAddress,
+                  controller: emailController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty){
+                      return 'please enter your email';
+                    } else if (value.contains('@')){
+                      return 'please enter a valid email';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'E-Mail',
+                    hintStyle: AppTextStyle.poppinsRegular.copyWith(
+                      fontSize: 14,
+                      color: Colors.black.withOpacity(0.6),
                     ),
-                    SizedBox(width: 10),
-                    Text('Sign-In with Google',
-                      style: AppTextStyle.poppinsMedium.copyWith(
-                        fontSize: 18,
-                        color: Colors.black,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 20,
+                    ),
+                    prefixIcon: Icon(Icons.person, size: 26),
+                  ),
+                ),
+                SizedBox(height: 14),
+                TextFormField(
+                  style: AppTextStyle.poppinsRegular.copyWith(fontSize: 14),
+                  controller: passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty){
+                      return'please enter your password';
+                    }
+                    else if (value.length<8){
+                      return'password must be at least 8 characters';
+                    }
+                    return null;
+                  },
+                  obscureText: isObscure,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: AppTextStyle.poppinsRegular.copyWith(
+                      fontSize: 14,
+                      color: Colors.black.withOpacity(0.6),
+                    ),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 10,
+                      vertical: 20,
+                    ),
+                    prefixIcon: Icon(Icons.fingerprint, size: 26),
+                    suffixIcon: IconButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () {
+                        setState(() {
+                          isObscure = !isObscure;
+                        });
+                      },
+                      icon: Icon(isObscure ? Icons.visibility_off : Icons.visibility),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+                Align(
+                  alignment:Alignment.centerRight,
+                  child: TextButton(onPressed: () {}, child: Text('Forgot Password?',
+                    style: AppTextStyle.poppinsMedium.copyWith(
+                      color: Colors.blue,
+                      fontSize: 16,
+                    ),
+                  ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                    minimumSize: Size(double.infinity,56),
+                  ),
+                  onPressed: () {
+                  if (formKey.currentState!.validate()){
+                  }
+                  },
+                  child: Text('Login'.toUpperCase(),
+                    style: AppTextStyle.poppinsMedium.copyWith(
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'or'.toUpperCase(),
+                    style: AppTextStyle.poppinsRegular.copyWith(
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                    ),
+                    minimumSize: Size(double.infinity,60),
+                    side: BorderSide(color: Colors.black,width: 1.5),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment:MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/images/google-logo.png',
+                        width: 26,
+                        height: 26,
                       ),
+                      SizedBox(width: 10),
+                      Text('Sign-In with Google',
+                        style: AppTextStyle.poppinsMedium.copyWith(
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account?',
+                        style: AppTextStyle.poppinsMedium.copyWith(
+                          fontSize: 18,
+                          color: Colors.black,
+                        )
+                    ),
+                    SizedBox(width: 8),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context,AppRoute.signup );
+                      },
+                    child: Text(
+                        'Signup',
+                        style: AppTextStyle.poppinsMedium.copyWith(
+                          fontSize: 18,
+                          color: Colors.blue,
+                        )
                     ),
                   ],
                 ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text('Don\'t have an account?',
-                      style: AppTextStyle.poppinsMedium.copyWith(
-                        fontSize: 18,
-                        color: Colors.black,
-                      )
-                  ),
-                  SizedBox(width: 8),
-                  Text('Signup',
-                      style: AppTextStyle.poppinsMedium.copyWith(
-                        fontSize: 18,
-                        color: Colors.blue,
-                      )
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
