d91e967f1a9a34cd8d02f368bf55900fc7c89972
aiera2876 <aiera2876@gmail.com>
2025-12-03 09:15:52 +0545

    loginactivity

--- lib/modules/home/bottom_navigation.dart
+++ lib/modules/home/bottom_navigation.dart
@@ -19,6 +19,25 @@ class _BottomNavigationState extends State<BottomNavigation> {
     return Scaffold(
       extendBody: true,
       body: screens[indexs],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: indexs,
+        onTap: (value) {
+          setState(() {
+            indexs = value;
+          });
+        },
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
+        selectedIconTheme: IconThemeData(
+          color: Colors.blue
+        ),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: 'Favorites'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+        ],
+      ),
       );
 
   }
