5f11b682a77203cf0f948307f61cebcbaea243ce
aiera2876 <aiera2876@gmail.com>
2025-12-05 08:42:56 +0545

    loginactivity

--- lib/modules/home/order_screen.dart
+++ lib/modules/home/order_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amine/constants/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class OrderScreen extends StatefulWidget {
@@ -11,8 +12,36 @@ class _OrderScreenState extends State<OrderScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('OrderScreen'),
+      appBar: AppBar(toolbarHeight: 60,),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 16,
+          right: 16,
+          bottom: 16,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Recent Orders' , style: AppTextStyle.poppinsSemiBold.copyWith(fontSize: 20),
+            ),
+            Text('Below are Your most Recent Orders', style: AppTextStyle.poppinsMedium.copyWith(fontSize: 21),
+            ),
+            SizedBox(height: 16),
+            ListView(
+              shrinkWrap: true,
+              primary: false,
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     )
   }
